241f20611c91a4c7964e586be10771e02a0d8abb
Hellopradeep69 <Hellopradeep69@gmail.com>
2026-06-26 21:31:31 +0530

    Oil and no lsp recipe

--- init.lua
+++ init.lua
@@ -43,7 +43,7 @@ vim.opt.path:append({ "**" })
 vim.diagnostic.config({ virtual_text = true })
 
 -- KEYMAPS
-vim.keymap.set("n", "<leader>fe", "q:i:fin ", {})
+vim.keymap.set("n", "<leader>fe", ":fin ", {})
 vim.keymap.set("n", "<leader>/", ":Ggrep | copen<left><left><left><left><left><left><left>", {})
 vim.keymap.set("n", "<leader>e", "<cmd>Lex<cr>", {})
 
@@ -58,18 +58,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	callback = function()
 		vim.hl.on_yank()
-	end,
-})
-
-vim.api.nvim_create_autocmd("PackChanged", {
-	callback = function(ev)
-		local name, kind = ev.data.spec.name, ev.data.kind
-		if name == "nvim-treesitter" and kind == "update" then
-			if not ev.data.active then
-				vim.cmd.packadd("nvim-treesitter")
-			end
-			vim.cmd("TSUpdate")
-		end
 	end,
 })
 
@@ -113,14 +101,12 @@ vim.cmd("packadd! nvim.undotree")
 vim.cmd("packadd! nvim.difftool")
 
 vim.pack.add({
+
+	{ src = "https://github.com/nvim-mini/mini.completion" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 	{ src = "https://gitlab.com/motaz-shokry/gruvbox.nvim" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-	{ src = "https://github.com/rafamadriz/friendly-snippets" },
-	{ src = "https://github.com/saghen/blink.cmp", version = vim.version.range("1.*") },
 	{ src = "https://github.com/nvim-mini/mini.pairs" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/tpope/vim-fugitive" },
 })
@@ -138,46 +124,7 @@ require("gruvbox").setup({
 	},
 })
 
-vim.cmd("colorscheme gruvbox")
-
-require("mason").setup()
-require("mason-lspconfig").setup()
-
-vim.diagnostic.config({ virtual_text = true })
-
-require("blink.cmp").setup({
-	keymap = { preset = "default" },
-
-	completion = {
-		documentation = {
-			auto_show = true,
-		},
-		menu = {
-			scrollbar = false,
-		},
-	},
-
-	sources = {
-		default = { "lsp", "path", "snippets", "buffer" },
-	},
-
-	signature = { enabled = true },
-	fuzzy = { implementation = "prefer_rust_with_warning" },
-})
-
-require("nvim-treesitter").install({
-	"bash",
-	"c",
-	"cpp",
-	"diff",
-	"lua",
-	"luadoc",
-	"luap",
-	"make",
-	"python",
-	"vim",
-	"vimdoc",
-})
+-- vim.cmd("colorscheme gruvbox")
 
 require("oil").setup({
 	default_file_explorer = false,
